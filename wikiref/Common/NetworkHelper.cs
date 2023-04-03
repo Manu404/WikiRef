e88b032ad77644ca14770bbbdb99cba2923b3011
Manu404 <contact@emmanuelistace.be>
2023-04-03 03:29:47 +0200

    Add ipv4 cookie less client option

--- wikiref/Common/NetworkHelper.cs
+++ wikiref/Common/NetworkHelper.cs
@@ -1,80 +1,89 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WikiRef
 {
     internal class NetworkHelper
     {
-        private HttpClient _httpClient;
         private ConsoleHelper _console;
+
+        private HttpClient _httpClient;
+        private HttpClient _httpClientCookieless;
+        Func<SocketsHttpConnectionContext, CancellationToken, ValueTask<Stream>> _ipv4ConnetionCallback;
+
         public NetworkHelper(ConsoleHelper console)
         {
+            // source: https://www.meziantou.net/forcing-httpclient-to-use-ipv4-or-ipv6-addresses.htm
+            _ipv4ConnetionCallback = async (context, cancellationToken) =>
+            {
+                // Use DNS to look up the IP addresses of the target host:
+                // - IP v4: AddressFamily.InterNetwork
+                // - IP v6: AddressFamily.InterNetworkV6
+                // - IP v4 or IP v6: AddressFamily.Unspecified
+                // note: this method throws a SocketException when there is no IP address for the host
+                var entry = await Dns.GetHostEntryAsync(context.DnsEndPoint.Host, AddressFamily.InterNetwork, cancellationToken);
+
+                // Open the connection to the target host/port
+                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+                // Turn off Nagle's algorithm since it degrades performance in most HttpClient scenarios.
+                socket.NoDelay = true;
+
+                try
+                {
+                    await socket.ConnectAsync(entry.AddressList, context.DnsEndPoint.Port, cancellationToken);
+                    return new NetworkStream(socket, ownsSocket: true);
+                }
+                catch
+                {
+                    socket.Dispose();
+                    throw;
+                }
+            };
+
             _httpClient = new HttpClient(new SocketsHttpHandler()
             {
-                ConnectCallback = async (context, cancellationToken) =>
-                {
-                    // Use DNS to look up the IP addresses of the target host:
-                    // - IP v4: AddressFamily.InterNetwork
-                    // - IP v6: AddressFamily.InterNetworkV6
-                    // - IP v4 or IP v6: AddressFamily.Unspecified
-                    // note: this method throws a SocketException when there is no IP address for the host
-                    var entry = await Dns.GetHostEntryAsync(context.DnsEndPoint.Host, AddressFamily.InterNetwork, cancellationToken);
-
-                    // Open the connection to the target host/port
-                    var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-
-                    // Turn off Nagle's algorithm since it degrades performance in most HttpClient scenarios.
-                    socket.NoDelay = true;
-
-                    try
-                    {
-                        await socket.ConnectAsync(entry.AddressList, context.DnsEndPoint.Port, cancellationToken);
-
-                        // If you want to choose a specific IP address to connect to the server
-                        // await socket.ConnectAsync(
-                        //    entry.AddressList[Random.Shared.Next(0, entry.AddressList.Length)],
-                        //    context.DnsEndPoint.Port, cancellationToken);
-
-                        // Return the NetworkStream to the caller
-                        return new NetworkStream(socket, ownsSocket: true);
-                    }
-                    catch
-                    {
-                        socket.Dispose();
-                        throw;
-                    }
-                }
+                ConnectCallback = _ipv4ConnetionCallback
             });
 
-            ConfigureHttpClientHeaders();
+            _httpClientCookieless = new HttpClient(new SocketsHttpHandler()
+            {
+                UseCookies = false,
+                ConnectCallback = _ipv4ConnetionCallback
+            });
+
+            ConfigureHttpClientHeaders(_httpClient);
+            ConfigureHttpClientHeaders(_httpClientCookieless);
 
             _console = console;
         }
 
-        private void ConfigureHttpClientHeaders()
+        private void ConfigureHttpClientHeaders(HttpClient httpclient)
         {
-            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0");
-            _httpClient.DefaultRequestHeaders.Add("Accept", "*/*");
-            _httpClient.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
-            _httpClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
-            _httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "document");
-            _httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
-            _httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Site", "none");
-            _httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Use", "?1");
-            _httpClient.DefaultRequestHeaders.Add("Sec-GPC", "1");
-            _httpClient.DefaultRequestHeaders.Add("TE", "trailers");
-            _httpClient.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+            httpclient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0");
+            httpclient.DefaultRequestHeaders.Add("Accept", "*/*");
+            httpclient.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
+            httpclient.DefaultRequestHeaders.Add("Connection", "keep-alive");
+            httpclient.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "document");
+            httpclient.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
+            httpclient.DefaultRequestHeaders.Add("Sec-Fetch-Site", "none");
+            httpclient.DefaultRequestHeaders.Add("Sec-Fetch-Use", "?1");
+            httpclient.DefaultRequestHeaders.Add("Sec-GPC", "1");
+            httpclient.DefaultRequestHeaders.Add("TE", "trailers");
+            httpclient.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
         }
 
-        public async Task<string> GetContent(string url)
+        public async Task<string> GetContent(string url, bool cookieLess = false)
         {
             string result = String.Empty;
             try
             {
-                using HttpResponseMessage response = await _httpClient.GetAsync(url);
+                using HttpResponseMessage response = cookieLess ? await _httpClientCookieless.GetAsync(url) : await _httpClient.GetAsync(url);
                 using HttpContent content = response.Content;
                 return await content.ReadAsStringAsync();
             }
@@ -94,7 +103,7 @@ namespace WikiRef
             return result;
         }
 
-        public async Task<string> GetYoutubeContent(string url)
+        public async Task<string> GetYoutubeShortContent(string url)
         {
             // required cookie to bypass "accept cookie screen". Disabling cookie on the httpclient solve the solution, but create problems on other websites.
             // Validity of current cookies
