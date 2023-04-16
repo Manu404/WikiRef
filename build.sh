ee08270cf1cc27e66f3a501a5e0e7f5be6252378
Manu404 <contact@emmanuelistace.be>
2023-04-16 09:50:09 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#rm-rf ./wikiref/obj/
-target="one"
 projectVersion="1.0.0.0"
 projetFile="./wikiref/WikiRef.csproj"
 projectName="wikiref"
@@ -10,88 +8,4 @@ buildScript="multiplateform_build.sh"
 rm -f $buildScript
 curl https://raw.githubusercontent.com/Manu404/MultiplateformDotNetCoreBuildScript/main/multiplateform_build.sh --output $buildScript
 chmod 755 $buildScript
-
-#
-# PARSE COMMAND PARAMETER
-#
-VALID_ARGS=$(getopt -o ase -l all:single:embedded -- "$@")
-if [[ $? -ne 0 ]]; then
-    exit 1;
-fi
-
-eval set -- "$VALID_ARGS"
-while [ : ]; do
-  case "$1" in
-    -a | --all)
-		target="all"
-        echo "Build all plateform"
-        break 
-        ;;
-	-s | --single)
-		target="one"
-        echo "Build sepecitific plateform"
-        break 
-        ;;
-	-e | --embedded)
-		embeded=true
-        echo "Build sepecitific plateform"
-        break 
-        ;;
-    --) shift; 
-        break 
-        ;;
-  esac
-done
-
-#
-# BUILD ALL SUPPORTED TARGET PORTABLE AND SINGLE FILE
-#
-if [ $target = "all" ]
-then
-	array=("win-x64" "win-x86" "win-arm64" "linux-x64" "linux-arm" "linux-arm64")
-	echo "List of plateforme: ${array[*]}"
-	for i in ${array[@]}
-	do
-		echo "Target platforme: '$i'"	
-		./$buildScript -p $projetFile -n $projectName -v $projectVersion -t "$i"
-		./$buildScript -p $projetFile -n $projectName -v $projectVersion -t "$i" -e
-	done
-	exit
-fi
-
-#
-# REQUEST FOR PORTABLE OR MULTIFILE
-#
-if [ $target = "one" ] && [ ! -n "$embeded" ]
-then
-	echo "Portable or multifile ?"
-	PS3="Your choice: "
-	kind=("portable" "multi")
-	select p in "${kind[@]}"; do
-		case $p in
-			"portable")
-				embed=true;
-				break;
-				;;
-			"multi")
-				embed=false;
-				break;
-				;;
-			*)
-		esac
-	done
-fi
-
-if [ $target = "one" ]
-then
-	#
-	# REQUEST COMPILATION
-	#
-	if [ -n $embeded ] && $embedded
-	then
-		./$buildScript -p $projetFile -n $projectName -v $projectVersion -e
-	else
-		./$buildScript -p $projetFile -n $projectName -v $projectVersion
-	fi	
-fi
-			
+./$buildScript -p $projetFile -n $projectName -v $projectVersion	
