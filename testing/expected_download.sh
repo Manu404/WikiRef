ea635b797d8295477571d9f449c5a9d9c9c35f00
Manu404 <contact@emmanuelistace.be>
2023-04-29 03:23:25 +0200

    Add icon + Logo
    update logo
    
    color correction - work was done with night light on '--
    
    add icon full
    
    update icon and add ico file
    
    add psd
    
    improve automatic testing, add download script testing
    
    add logo
    
    add logo 2
    
    add logo

---
+++ testing/expected_download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\USUL_S_ENFLAMME_ET_PARLE_DES_METIER_DE_MAYRDE_DES_PETIT_PROLO__[TH9KCJgPeZM].mp4" https://www.youtube.com/watch?v=TH9KCJgPeZM
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\VELMA_-_LA_PIRE_SERIE_WOKE_SE_FAIT_VAPORISÉ_PAR_LES_CRITIQUE__[ZsHUweqUJ1A].mp4" https://www.youtube.com/watch?v=ZsHUweqUJ1A
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\PSYHODELIK_DISCUTE_AVEC_UN_MILITANT_TRANS-LGBT_SUR_QU_EST-CE_QU_UNE_FEMME___-_FEAT_@ZioCloaPX__[iEHd1YtIRK8].mp4" https://www.youtube.com/watch?v=iEHd1YtIRK8
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\DANY_CALIGULA_DANS_LA_SAUCE_POUR_AVOIR_DIT_QUE_SAMUEL_PATY_AVAIT_CHIIER_SUR_L_ISLAM__[KlaRbENAC8E].mp4" https://www.youtube.com/watch?v=KlaRbENAC8E
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\DISNEY_SENSURE_PICSOU_A_CAUSE_DE_CA_____[O933lKdjXlQ].mp4" https://www.youtube.com/watch?v=O933lKdjXlQ
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\UN_NAGEUR_TRANS_FINI_DERNIER_MAIS_CE_SENT_ACCOMPLIS_EN_TANT_QU_HOMME__[zwklYb5m_s0].mp4" https://www.youtube.com/watch?v=zwklYb5m_s0
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\LE_PREMIER_PERSO_TRANS_DE_HOGWARTS_LEGACY_EST_AKHUZÉ_DE_TRANSFOUBY__[_6A1SzCvTWo].mp4" https://www.youtube.com/watch?v=_6A1SzCvTWo
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\JE_DEBUNK_MA_PAGE_POLITIWIKI_FAITES_PAR_BADMULCH__[zI6laqkcDVQ].mp4" https://www.youtube.com/watch?v=zI6laqkcDVQ
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\UN_HOMME_TRANSGENRE_DONNE_NAISSANCE_AU_BÉBÉ_D_UN_PARTENAIRE_NON_BINAIRE_AVEC_UNE_DONNEUSE_DE_SPAIRME__[44mP1zP7zQ0].mp4" https://www.youtube.com/watch?v=44mP1zP7zQ0
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\UNE_LOI_POUR_METTRE_DES_TAMPON_DANS_LES_TOILETTES_DES_GARCONS__[IMuc4O3xkno].mp4" https://www.youtube.com/watch?v=IMuc4O3xkno
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\GAMEKULT_DÉCIDE_DE_KHANCEL_HOGWARTS_LEGACY_ET_REFUSE_DE_FAIRE_LE_TEST_A_CAUSE_DE_JK_ROWLING__[KQBq_dUzszw].mp4" https://www.youtube.com/watch?v=KQBq_dUzszw
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\PSYHODELIK_SE_CONTREDIT_TOUT_SEUL_ET_SE_BLESSE_DANS_SA_CONFUSION_!__[o0jQKX4COR8].mp4" https://www.youtube.com/watch?v=o0jQKX4COR8
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\LE_MOT__TERRAIN__EST_JUGÉ_OFENSSANT_ET_ANTI-NOIR__[lX3cvG9wUuk].mp4" https://www.youtube.com/watch?v=lX3cvG9wUuk
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\PANTIN_DEVIENT_PANTINE_POUR_SOUTENIR_L_ÉGALITÉ_HOMMES-FEMMES__[iLnmw_yjxYE].mp4" https://www.youtube.com/watch?v=iLnmw_yjxYE
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\Psyho_et_KB_VOUS_MANIPULENT_ET_L_AVOUENT__[PElWZRmFwc4].mp4" https://www.youtube.com/watch?v=PElWZRmFwc4
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\LA_MANGUE_C_EST_RACISTE_🥭__[Sve0-gSyzME].mp4" https://www.youtube.com/watch?v=Sve0-gSyzME
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Psyhodelik\PSYHODELIK_EST_PRÉVISIBLE_ET_FACILE_A_PIÉGER_!__[IUYQgRmhFEg].mp4" https://www.youtube.com/watch?v=IUYQgRmhFEg
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Testpagewikiref\Elon_Musk_fires_employees_in_twitter_meeting_DUB__[2j3p_aDMTNg].mp4" https://www.youtube.com/watch?v=2j3p_aDMTNg
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Testpagewikiref\__[yZ-K7nCVnBI].mp4" https://www.youtube.com/watch?v=yZ-K7nCVnBI
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Testpagewikiref\Rick_Astley_-_Never_Gonna_Give_You_Up_(Official_Music_Video)__[dQw4w9WgXcQ].mp4" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Testpagewikiref\__[watch].mp4" https://www.youtube.com/watch?v=watch
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Testpagewikiref\How_Wings_Are_Attached_to_the_Backs_of_Angels__[NRHVzbJVx8I].mp4" https://www.youtube.com/watch?v=NRHVzbJVx8I
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Testpagewikiref\VGA_2011__Diablo_3_Exclusive_Intro_Cinematic__[nas1rJpm7wY].mp4" https://www.youtube.com/watch?v=nas1rJpm7wY
+/usr/local/bin/yt-dlp -S res,ext:mp4:m4a --recode mp4 -o "./video\Testpagewikiref\Test_Your_Awareness___Whodunnit___[ubNF9QNEQLA].mp4" https://www.youtube.com/watch?v=ubNF9QNEQLA
+
--- testing/main.sh
+++ testing/main.sh
@@ -15,22 +15,32 @@ outputFolder=$(date)
 version=1.0.0.0
 targetwiki="http://192.168.2.112"
 expected_json=$(pwd)"/expected_output.json"
-
+expected_script=$(pwd)"/expected_download.sh"
+vm_name=""
+vm_ip=""
+vm_snapshot=""
+vm_code=""
 
 mkdir -p "$outputFolder"
 
 function waitingForKeyPress(){
+	echo -e "$Red"
 	read -p "Press any key to continue... " -n1 -s
+	echo -e "$Color_Off"
 }
 
 #
 # TESTING SERVER
 #
-
 vm_name="WikiRef MediaWiki"
-#restor mediawiki vm
-VBoxManage.exe snapshot "$vm_name" restore "fresh"
-#start vm
+vm_snapshot="fresh"
+echo -e  "$Cyan#> ===================================================================== $Color_Off"
+echo -e  "$Cyan#>                             VM $vm_name                               $Color_Off"
+echo -e  "$Cyan#> ===================================================================== $Color_Off"
+echo -e  "$Cyan#> Apply snapshot $vm_snapshot for $vm_name $Color_Off" 
+VBoxManage.exe snapshot "$vm_name" restore "$vm_snapshot"
+
+echo -e  "$Cyan#> Start $vm_name $Color_Off"
 VBoxManage.exe startvm "$vm_name"
 
 #
@@ -40,55 +50,87 @@ function Test() {
 	vm_name=$1
 	vm_ip=$2
 	vm_snapshot=$3
+	vm_code=$4
 	
-	echo "$Blue#> Start test $Color_Off" 
+	echo -e  "$Cyan#> ===================================================================== $Color_Off"
+	echo -e  "$Cyan#>                             VM $vm_name                               $Color_Off"
+	echo -e  "$Cyan#> ===================================================================== $Color_Off"
+	
+	echo -e  "$Cyan#> start test $Color_Off" 
 	waitingForKeyPress
 
-	echo "$Blue#> Apply snapshot $vm_snapshot for $vm_name$Color_Off" 
-	VBoxManage.exe snapshot "$vm_name" restore "$vm_snapshot"
+	echo -e  "$Cyan#> apply snapshot $vm_snapshot for $vm_name $Color_Off" 
+	vboxmanage.exe snapshot "$vm_name" restore "$vm_snapshot"
 
-	echo "$Blue#> Start $vm_name$Color_Off"
-	VBoxManage.exe startvm "$vm_name"
+	echo -e  "$Cyan#> start $vm_name $Color_Off"
+	vboxmanage.exe startvm "$vm_name"
 
 	waitingForKeyPress
 
-	echo "$Blue#> Recreate distant wikiref dir$Color_Off"
+	echo -e  "$Cyan#> recreate distant wikiref dir $Color_Off"
 	sshpass -p 1234 ssh eis@$vm_ip 'rm -rf wikiref && mkdir wikiref'
 
-	echo "$Blue#> Upload wikiref file$Color_Off"
+	echo -e  "$Cyan#> upload wikiref file $Color_Off"
 	zip_filename="wikiref_""$version""_linux-x64_portable.zip"
 	zip_location="..\\output\\zip\\""$zip_filename"
 	sshpass -p 1234 scp $zip_location eis@$vm_ip:/home/eis/wikiref/wikiref.zip
 
-	echo "$Blue#> Unzip wikiref$Color_Off"
+	echo -e  "$Cyan#> unzip wikiref $Color_Off"
 	sshpass -p 1234 ssh eis@$vm_ip 'cd wikiref && unzip wikiref.zip && chmod +x wikiref'
 
-	echo "$Blue#> Analyse$Color_Off"
-	sshpass -p 1234 ssh eis@$vm_ip "cd wikiref && ./wikiref analyse -w $targetwiki -c \"Test pages\" --json output.json -b -l"
+	echo -e  "$Cyan#> analyse $Color_Off"
+	sshpass -p 1234 ssh eis@$vm_ip "cd wikiref && ./wikiref analyse -w $targetwiki -c \"test pages\" --json output.json -b -l"
 	
-	echo "$Blue#> Generate download script$Color_Off"
-	sshpass -p 1234 ssh eis@$vm_ip "cd wikiref && ./wikiref analyse -w $targetwiki -c \"Test pages\" --json output.json -b -l"
-
-	echo "$Blue#> Download json$Color_Off"
-	outputJson=$(pwd)"/$outputFolder/output_ubuntu1804.json"
+	echo -e  "$Cyan#> build fake video $Color_Off"
+	sshpass -p 1234 ssh eis@$vm_ip "cd wikiref && mkdir -p ./video/Psyhodelik"
+	sshpass -p 1234 ssh eis@$vm_ip "cd wikiref && touch ./video/Psyhodelik/LE_PREMIER_PRETRE_NON_BINAIRE_GRACE_A_ADAM_ET_EVE__[E0gzvUc797I].mp4"
+	sshpass -p 1234 ssh eis@$vm_ip "cd wikiref && touch ./video/Psyhodelik/#RDF_Psyhodelik_clash__La_gauchiasse!__[gcMm2zzUP6s].mp4"
+	
+	echo -e  "$Cyan#> generate download script $Color_Off"
+	sshpass -p 1234 ssh eis@$vm_ip "cd wikiref && ./wikiref script -i output.json --tool /usr/local/bin/yt-dlp -d ./video --output-script download_script.sh"
+		
+	echo -e  "$Cyan#> download json $Color_Off"
+	output_json=$(pwd)"/$outputFolder/output_$vm_code.json"
 	sshpass -p 1234 scp eis@$vm_ip:/home/eis/wikiref/output.json _output.json
-	mv _output.json "$outputJson"
+	mv _output.json "$output_json"
 	
+	echo -e  "$Cyan#> download download script $Color_Off"
+	output_script=$(pwd)"/$outputFolder/download_$vm_code.sh"
+	echo $output_script
+	sshpass -p 1234 scp eis@$vm_ip:/home/eis/wikiref/download_script.sh _download.json
+	mv _download.json "$output_script"
 
-	echo "$Blue#> Convert EOL$Color_Off"
-	unix2dos "$outputJson"
+	echo -e  "$Cyan#> convert eol $Color_Off"
+	unix2dos "$output_json"
+	unix2dos "$output_script"
+	
+	echo $output_script
+	echo $expected_script
+	echo -e  "$Cyan#> Diff $output_script and $expected_script $Color_Off"
+	if [ "$(wc -l < "$output_script")" -eq "$(wc -l < $expected_script)" ]; 
+	then 
+		echo -e "$Green#> Download file contains the same number of downloads$Color_Off"; 
+	else 
+		echo -e "$Red#> Differenre number of lines$Color_Off"; 
+		echo -e "$Red#> Expected: $(wc -l < $expected_script)$Color_Off"; 
+		echo -e "$Red#> Provided: $(wc -l < "$output_script")$Color_Off"; 
+	fi
 
-	echo "$Blue#> Diff $outputJson and $expected_json$Color_Off"
-	diff -q "$outputJson" "$expected_json"
+	echo -e  "$Cyan#> Diff $outputJson and $expected_json $Color_Off"
+	diff -q "$output_json" "$expected_json"
 	if [[ $? == "0" ]]
 	then
-	  echo "$GreenThe same$Color_Off"
+	  echo -e  "$Green#> JSON Files are identical $Color_Off"
 	else
-	  echo "$RedNot the same$Color_Off"
+	  echo -e  "$Red#> JSON Files are different $Color_Off"
 	fi
 	
-	echo "$Blue#> Shutdown $vm_name$Color_Off"
-	sshpass -p 1234 ssh eis@$vm_ip 'shutdown -h now'
+	echo -e  "$Cyan#> Shutdown $vm_name $Color_Off"
+	vboxmanage.exe controlvm "$vm_name" acpipowerbutton
+	
+	echo -e  "$Cyan#> $Color_Off"
+	echo -e  "$Cyan#> --------------------------------------------------------------------- $Color_Off"
+	echo -e  "$Cyan#> $Color_Off"
 }
 
-Test "Ubuntu Server 18.04" "192.168.2.103" "fresh"
+Test "Ubuntu Server 18.04" "192.168.2.103" "fresh" "ubuntu_1804"
