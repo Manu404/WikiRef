13c79ef79d8240cacef0b02555c72db902b0754a
Manu404 <contact@emmanuelistace.be>
2023-05-02 19:57:42 +0200

    fix build script

---
+++ build/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+projectVersion="1.0.0.0"
+projetFile="./WikiRef/WikiRef.csproj"
+projectName="wikiref"
+buildScript="multiplateform_build.sh"
+
+chmod 755 $buildScript
+cd ../
+./build/MultiplateformDotNetCoreBuildScript/$buildScript -p $projetFile -n $projectName -v $projectVersion	
